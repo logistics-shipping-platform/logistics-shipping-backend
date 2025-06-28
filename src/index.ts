c280c7056829f7fbac8574810216e106d7cf53ce
Daniel Tobon <danieltu1026@gmail.com>
2025-06-28 16:34:07 -0500

    Se agrega cors para poder recibir peticiones del frontend

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import { pool } from './config/MysqlDatabase';
+import cors from 'cors';
 
 import { MySQLUserRepo } from './adapter/outbound/persistence/MysqlUserRepo';
 import {
@@ -20,6 +21,12 @@ async function main() {
   const app = express();
   const port = process.env.API_GATEWAY_PORT || 3000;
   app.use(express.json());
+  app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
   // Adapters outbound
   const userRepo = new MySQLUserRepo();
@@ -33,8 +40,8 @@ async function main() {
   // Controllers
   const authCtrl = new AuthController(authenticateUserUC);
   const userCtrl = new UserController(registerUC);
-  app.post('/auth', authCtrl.login);
-  app.post('/user', userCtrl.register);
+  app.post('/api/auth/login', authCtrl.login);
+  app.post('/api/auth/register', userCtrl.register);
 
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
