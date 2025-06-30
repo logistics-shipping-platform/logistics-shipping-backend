a40ad1a86e185711074ab336ee0042f9fdbad1d0
Daniel Tobon <danieltu1026@gmail.com>
2025-06-30 17:14:51 -0500

    Se corrige la duplicacion de elememntos en el historia de estados en la creacion del envio

--- src/adapter/inbound/http/AuthController.ts
+++ src/adapter/inbound/http/AuthController.ts
@@ -5,6 +5,11 @@ import { AuthenticateUserUseCase } from '../../../application/usecase';
 export class AuthController {
   constructor(private authUC: AuthenticateUserUseCase) {}
 
+  /**
+   * Maneja la solicitud de inicio de sesión.
+   * @param req - La solicitud HTTP que contiene las credenciales del usuario.
+   * @param res - La respuesta HTTP que se enviará al cliente.
+   */
   login = async (req: Request, res: Response) => {
     try {
       const { token } = await this.authUC.execute(req.body);
